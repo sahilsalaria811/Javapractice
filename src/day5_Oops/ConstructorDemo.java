66786e9c38e9d0c922548ea4fd799232a007e315
Sahil salariasahilsalaria811 <sahilsalaria811@gmail.com>
2023-05-04 19:43:03 +0530

    modify the constructor

---
+++ src/day5_Oops/ConstructorDemo.java
@@ -0,0 +1,37 @@
+package day5_Oops;
+
+public class ConstructorDemo {
+
+	// 2 TYPES OF CONSTRUCTOR
+//	    i)Default CONSTRUCTOR
+	// ii)Parameterised CONSTRUCTOR
+
+	int x;
+	int y;
+
+	ConstructorDemo()    //	    Default CONSTRUCTOR
+	{
+		x = 20;
+		y = 256;
+	}
+
+	ConstructorDemo(int a, int b)   // Parameterised CONSTRUCTOR
+	{
+		x = a;
+		y = b;
+	}
+	
+
+	void display() {
+		System.out.println(x + y);
+	}
+
+	public static void main(String[] args) {
+		ConstructorDemo cm = new ConstructorDemo();
+        cm.display();
+          	
+	     ConstructorDemo cm1 = new ConstructorDemo(10,20);
+	     cm1.display(	);
+	}
+
+}
