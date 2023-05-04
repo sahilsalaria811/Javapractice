7f1730b26e0e65f4f9bd58fa50417d09176ef661
Sahil salariasahilsalaria811 <sahilsalaria811@gmail.com>
2023-05-04 19:32:59 +0530

    Constructor

--- src/day5_Oops/Student.java
+++ src/day5_Oops/Student.java
@@ -1,31 +1,34 @@
 package day5_Oops;
 
 public class Student {
-	
+
 	int sid;
 	String sname;
 	char grade;
-	
 
-	Student(int id, String name, char g) //constructor 
+	// Void method to print the value
+	void display() {
+		System.out.println("Sid= " + sid + " Sname= " + sname + " grade= " + grade);
+	}
+
+	// 2> Create a void method to call in th main class assign the value
+	void getvalues(int id, String name, char g) {
+		sid = id;
+		sname = name;
+		grade = g;
+	}
+
+	// 1) Constructor is a special type of method
+	// 2) Constructor is used for initilizing the class variable
+	// 3) Constructor name should be same as the class name
+	// 4) Constructor will not return any value(not even void)
+	// 5) Constructor will be invoked at the time of object creation.
+	Student(int id, String name, char g)
 	{
 		sid=id;
 		sname=name;
 		grade=g;
-	}
-	
-	void getvalues(int id,String name,char g) //method
-	{
-		sid =id ;
-		sname=name;
-		grade=g;
 		
-	}
-	
-	
-	void display()
-	{
-		System.out.println("Sid= " + sid +" Sname= " + sname + " grade= " + grade);
 	}
 	
 
