7b6c1d2eeff0915ffa223d093a46543ca15f502c
Sahil salariasahilsalaria811 <sahilsalaria811@gmail.com>
2023-05-09 18:41:32 +0530

    This keyboard

--- src/day6_Oops/ConstructorOverloading.java
+++ src/day6_Oops/ConstructorOverloading.java
@@ -1,27 +1,20 @@
 package day6_Oops;
 
 public class ConstructorOverloading {
-	
-	
-	ConstructorOverloading(int a, int b){
 
-		System.out.println(a+b);
-	
-	}
-	ConstructorOverloading(int a, double b)
-	{
-		System.out.println(a+b);
-		
-	}
-	
+	ConstructorOverloading(int a, int b) {
 
-	public static void main (String []args) {
-	
-	ConstructorOverloading obj= new ConstructorOverloading(10,10.12);
+		System.out.println(a + b);
+
+	}
+
+	ConstructorOverloading(int a, double b) {
+		System.out.println(a + b);
+
+	}
+
+	public static void main(String[] args) {
+
+		ConstructorOverloading obj = new ConstructorOverloading(10, 10.12);
 	}
 }
-	
-	
-	
-
-
---
+++ src/day6_Oops/ThisKeyword.java
@@ -0,0 +1,23 @@
+package day6_Oops;
+
+public class ThisKeyword {
+	int a, b;	 // instance variable or class variable 
+	void getvalues (int x, int y) 
+	{
+		a=x;
+		b=y;
+	}
+	void printvalues() 
+	{System.out.println(a);
+	System.out.println(b);}
+	
+	public static void main (String []args)
+	{
+		ThisKeyword th =new ThisKeyword();
+		th.getvalues(10,20);
+		th.printvalues();			
+	}	
+	
+	
+
+}
