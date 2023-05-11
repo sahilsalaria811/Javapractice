9a66f5f49e2eef1d6a7141cdb1812de39faa6f8b
Sahil salariasahilsalaria811 <sahilsalaria811@gmail.com>
2023-05-11 15:53:50 +0530

    Final keyword class

---
+++ src/day7_Oops/FinalMethodDemo.java
@@ -0,0 +1,5 @@
+package day7_Oops;
+
+public class FinalMethodDemo {
+
+}
---
+++ src/day7_Oops/FinalVariableDemo.java
@@ -0,0 +1,21 @@
+package day7_Oops;
+
+public class FinalVariableDemo {
+
+	int speed =40;
+	
+	
+	
+	
+	
+	
+	public static void main(String[] args) {
+	
+		FinalVariableDemo fm = new FinalVariableDemo();
+		fm.speed=100;
+		System.out.println(fm.speed);
+		
+		
+	}
+
+}
