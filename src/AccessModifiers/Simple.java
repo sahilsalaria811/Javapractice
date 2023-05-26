79e8562c5d8d27d63c7a260b70ba11b7ff74d8fc
Sahil salariasahilsalaria811 <sahilsalaria811@gmail.com>
2023-05-26 19:13:17 +0530

    access

--- src/AccessModifiers/Simple.java
+++ src/AccessModifiers/Simple.java
@@ -1,5 +1,35 @@
 package AccessModifiers;
 
+class A {
+	
+	
+	
+//	private int data = 10;
+//
+//	private void M1() {
+//		System.out.println(data);
+//	}
+
+	
+	
+	protected int a=20;
+	protected void m2() {
+		
+	}
+	
+	
+	
+	
+	
+}
+
 public class Simple {
 
+	public static void main(String[] args) {
+
+		A aobj = new A();
+
+	//	aobj.M1(); // Complile time error
+
+	}
 }
