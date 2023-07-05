4ffe6b384f0ba1c74601062b551a074165893f33
Sahil salariasahilsalaria811 <sahilsalaria811@gmail.com>
2023-07-05 19:37:11 +0530

    pack 1

--- src/AccessModifiers2/Sample.java
+++ src/AccessModifiers2/Sample.java
@@ -1,5 +1,17 @@
 package AccessModifiers2;
+import Pack1.ABC;
 
-public class Sample extends A {
+public class Sample extends ABC  {
 
+	public static void main(String []args) {
+	
+		Sample s= new Sample();
+	s.data=100;
+	s.m1();
+	
+	}
+	
+	
+	
+	
 }
---
+++ src/Pack1/ABC.java
@@ -0,0 +1,11 @@
+package Pack1;
+
+public class ABC {
+	
+	protected int data =20; 
+protected void m1() {
+	System.out.println(data);
+}
+	}
+
+
